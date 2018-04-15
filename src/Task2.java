fe9f0c95167950810e4688b9aef9f7eb7b2d55b5
blacklittle <b10004143@gmail.com>
2018-04-15 21:19:59 +0800

    將5,10,12行改成第二個level

--- src/Task2.java
+++ src/Task2.java
@@ -2,13 +2,13 @@
 public class Task2 {
 	public static void main(String[] args) {
 		int input = 5;
-			System.out.println("======== 我是分隔線 =========");
+		System.out.println("======== 我是分隔線 =========");
 		for (int x = 1; x <= input; x = x+1) {
 			for (int y = 1; y <= x; y = y+1) {
-				System.out.print("*");
+			System.out.print("*");
 			}
-			System.out.println();
+		System.out.println();
 		}
-			System.out.println("======== 我是分隔線 =========");
+		System.out.println("======== 我是分隔線 =========");
 	}
 }
