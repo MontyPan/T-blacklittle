ed5f1d63cb3d362d326f5ada34240a1489d195ac
blacklittle <b10004143@gmail.com>
2018-04-15 15:53:52 +0800

    修改 first for迴圈的換行位置以及縮排

--- src/task1.java
+++ src/task1.java
@@ -1,14 +1,12 @@
 
 public class task1 {
 	public static void main(String[] args) {
-		int input = 5;
-		for (int x = 1; x <= input; x = x+1) {
+		int input = 3;
+		for (int x = 0; x <= input; x = x+1) {
+			for (int y = 0; y <= input; y = y+1) {
+				System.out.print("*");
+			}
 			System.out.println();
-		for (int y = 1; y <= input; y = y+1) {
-			System.out.print ("*");
-			
-		
-		}
 		}
 	}
 }
