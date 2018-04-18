8bab54089b02122bef107daff9b86e9b3df70d7b
blacklittle <b10004143@gmail.com>
2018-04-18 22:50:13 +0800

    修改Task2之多行註解 增加task1分隔線及修改input數量

---
+++ src/Task2.java
@@ -0,0 +1,18 @@
+﻿
+public class Task2 {
+	public static void main(String[] args) {
+		int input = 5;
+		System.out.println("======== 我是分隔線 =========");
+		for (int x = 1; x <= input; x = x+1) {
+			for (int y = 1; y <= x; y = y+1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.print("======== 我是分隔線 =========");
+	}
+}
+/*.gitignore 是一個 git 用以紀錄忽略檔案的清單。
+ * .gitignore 存在的目的為減少檔案版本控制時的雜亂程度，將在執行 project 時產生暫存檔案或快取檔案自動忽略。
+ * 若無將這些檔案加入 gitignore 清單內，則這些檔案會被 git 視為 untracked file，當執行 git add . 會被加入到 local repo。
+ */
