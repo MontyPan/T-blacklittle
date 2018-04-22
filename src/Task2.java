3c4256b4973631762feed419524fd5e74d8baa08
blacklittle <b10004143@gmail.com>
2018-04-22 21:43:32 +0800

    Task2

---
+++ src/Task2.java
@@ -0,0 +1,14 @@
+﻿
+public class Task2 {
+	public static void main(String[] args) {
+		int input = 5;
+		System.out.println("======== 我是分隔線 =========");
+		for (int x = 1; x <= input; x = x+1) {
+			for (int y = 1; y <= x; y = y+1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.print("======== 我是分隔線 =========");
+	}
+}
