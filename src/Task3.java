f931d0de0b370371726eb5d4f1a17bb945d12294
blacklittle <b10004143@gmail.com>
2018-04-22 21:53:56 +0800

    Task3 已修改成 " 限制只能用兩個 for 迴圈 " 的版本

--- src/Task3.java
+++ src/Task3.java
@@ -4,18 +4,14 @@ public class Task3 {
 		int input = 5;
 		System.out.println("======== 我是分隔線 =========");
 		for (int x = 1; x <= 2*input-1; x = x+1) {
-			if (x > input) {
-				for (int y = 1; y <= 2*input-x; y = y+1) {
-					System.out.print("*");
-				}	
-				System.out.println();
+			int upper = x;
+			if (x>input) {
+				upper = 2*input-x;
 			}
-			else {
-				for (int y = 1; y <= x; y = y+1) {
-					System.out.print("*");
-				}	
-				System.out.println();
-			}
+			for (int y = 1; y <= upper; y = y+1) {
+				System.out.print("*");
+			}	
+			System.out.println();
 		}
 		System.out.print("======== 我是分隔線 =========");
 	}
