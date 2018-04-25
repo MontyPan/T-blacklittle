7999d9844aec8603a94b94325a545ff7e049f690
blacklittle <b10004143@gmail.com>
2018-04-25 21:30:11 +0800

    Task4 帽子星星(迷你裙版本)

--- src/Task4.java
+++ src/Task4.java
@@ -2,23 +2,15 @@
 public class Task4 {
 	public static void main(String[] args) {
 		int input = 5;
-		for (int x = 1; x <= input; x = x+1) {
-			for (int y = 0; y <= input-1; y = y+1) {
-				if(y>=x) {
-					System.out.print(" ");
+		for (int x = 0; x <= input-1; x = x+1) {
+			for (int y = 0; y <= 2*input-2; y = y+1) {
+				if(y<=x | y>2*input-x-3) {
+					System.out.print("*");
 				}
 				else {
-					System.out.print("*");
+					System.out.print(" ");
 				}
 			}			
-			for (int z = 1; z <= input-1; z = z+1) {	
-				if(z>input-x-1) {
-					System.out.print("*");
-				}
-				else {
-					System.out.print(" ");
-				}	
-			}	
 			System.out.println();
 		}
 		System.out.print("======== 我是分隔線 =========");
