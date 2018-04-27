73098d2a0bee1a0496dedbfa8e705cda4a85189a
blacklittle <b10004143@gmail.com>
2018-04-27 21:53:17 +0800

    兩個 for-loop 之第二版

--- src/Task3.java
+++ src/Task3.java
@@ -3,14 +3,13 @@ public class Task3 {
 	public static void main(String[] args) {
 		int input = 5;
 		System.out.println("======== 我是分隔線 =========");
-		for (int x = 1; x <= 2*input-1; x = x+1) {
-			int upper = x;
-			if (x>input) {
-				upper = 2*input-x;
+		for (int x = 0; x < 2*input-1; x = x+1) {
+			for (int y = 0; y < input; y = y+1) {
+				if(y<=x & y>x-input) {
+					System.out.print("*");
+				}
 			}
-			for (int y = 1; y <= upper; y = y+1) {
-				System.out.print("*");
-			}	
+				
 			System.out.println();
 		}
 		System.out.print("======== 我是分隔線 =========");
