950462d9675a92fef4f03f06f915b6a63d1a2d21
blacklittle <b10004143@gmail.com>
2018-04-27 20:26:25 +0800

    Task5 a 和 b 兩數之 LCD 及 GCM

---
+++ src/Task5.java
@@ -0,0 +1,21 @@
+﻿
+public class Task5 {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 15;
+		int gcd;
+		int lcm;
+		
+		//blacklittle：不會寫啦～ T__T
+		
+		for(int x=a; x>1; x--) {
+			if(a%x==0 && b%x==0) {
+				lcm=x;
+				gcd=a*b/lcm;
+				System.out.println(a + " 跟 " + b + " 的 LCM 為 " + lcm);
+				System.out.println(a + " 跟 " + b + " 的 GCD 為 " + gcd);
+				break;
+			}	
+		}
+	}
+}
