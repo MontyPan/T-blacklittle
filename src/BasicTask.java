4c28c45de2d3d45557a3b4ffbd775e886bfcbe4c
MontyPan <monty.pan@dontcareabout.us>
2018-04-29 21:43:49 +0800

    轉移 task-2 到 BasicTask

--- src/BasicTask.java
+++ src/BasicTask.java
@@ -8,4 +8,15 @@ public class BasicTask {
 			System.out.println();
 		}
 	}
+
+	public static void task2(int input) {
+		System.out.println("======== 我是分隔線 =========");
+		for (int x = 1; x <= input; x = x+1) {
+			for (int y = 1; y <= x; y = y+1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.print("======== 我是分隔線 =========");
+	}
 }
