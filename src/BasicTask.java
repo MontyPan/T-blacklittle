3253f4f3b58b247e529e03aff8dbc399088ebddf
MontyPan <monty.pan@dontcareabout.us>
2018-04-29 21:41:44 +0800

    轉移 task-1 到 BasicTask

--- src/BasicTask.java
+++ src/BasicTask.java
@@ -1,7 +1,6 @@
 
-public class task1 {
-	public static void main(String[] args) {
-		int input = 3;
+public class BasicTask {
+	public static void task1(int input) {
 		for (int x = 0; x <= input; x = x+1) {
 			for (int y = 0; y <= input; y = y+1) {
 				System.out.print("*");
